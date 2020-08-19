2749a6183fde325f06ad8174c53ad65ac185d241
Leo Massaroli <massaroli.leonardo@gmail.com>
2020-08-19 20:51:30 -0300

    responsive view + fix tab title

--- web_client/index.jsx
+++ web_client/index.jsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./components/app.jsx";
 
 window.onload = function () {
-  // Renderizamos la App
+  // Setting tab title
+  document.title = "Year Calendar";
+
+  // We render the app
   ReactDOM.render(<App />, document.getElementById("root"));
 };
